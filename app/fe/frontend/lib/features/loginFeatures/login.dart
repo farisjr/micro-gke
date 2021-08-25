237ad768e71aa2836abdf745ae8fa610a4e5bdf8
FarisJR <farisjramadhan@gmail.com>
2021-08-25 08:06:54 +0700

    add fe

---
+++ app/fe/frontend/lib/features/loginFeatures/login.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
